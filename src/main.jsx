c1ee5bc4f37a4e468b4613fc6e3a97fcc71a7660
mysticaltexts-source <mystical.texts@gmail.com>
2026-06-06 11:47:42 -0500

    Fix formatting of ReactDOM render method

--- src/main.jsx
+++ src/main.jsx
@@ -4,5 +4,6 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.createElement(React.StrictMode,null
-  <React.createElement(App,null
+  <React.createElement(App,null)
+)
 )
